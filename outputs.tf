9ea98dfb6a10c79b30d4b2736b6bb1f96df18426
oraclesean <oracle.sean@gmail.com>
2022-04-06 23:07:21 +0000

    Fix IP output display in RM

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "generated_ssh_private_key" {
   value = tls_private_key.public_private_key_pair.private_key_pem
   sensitive = true
 }
-output "PublicIPs" {
+output "lab_public_ip" {
   value = oci_core_instance.lab.*.public_ip
 }
