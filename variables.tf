b0544ce6976a3481e2a21f7cbbbadf7bf392e553
oraclesean <oracle.sean@gmail.com>
2022-04-06 21:42:32 +0000

    Cleanup variables

--- variables.tf
+++ variables.tf
@@ -7,23 +7,12 @@ variable "private_key_path" {}
 variable "availability_domain_name" {
   default     = null
 }
-#variable "is_always_free" {
-#  default     = false
-#}
 
 ## Compartment
-#variable "dedicated_compartment" {
-#  description = "Create a dedicated compartment"
-#  default     = false
-#}
 variable "lab_name" {
   description = "Compartment name"
   default     = "upgrade"
 }
-#variable "delete_compartment" {
-#  description = "Delete dedicated compartment"
-#  default     = false
-#}
 
 # Networking
 variable "vcn_cidr" {
@@ -33,17 +22,9 @@ variable "vcn_cidr" {
 variable "vcn_dns_label" {
   default     = "lab"
 }
-#variable "public_sn_display_name" {
-#  default     = "lab_public_subnet"
-#}
 variable "public_sn_dns_label" {
   default     = "lab"
 }
-
-# IP Assignment
-#variable "public_ip_display_name" {
-#  default     = "lab_public_ip"
-#}
 
 # Instance
 variable "instance_shape" {
